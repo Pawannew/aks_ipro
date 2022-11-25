f71ac2122545b85775d10aa8bae3ef996801d14d
technicalaks <105633032+technicalaks@users.noreply.github.com>
2022-11-25 22:14:00 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -239,7 +239,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('ᴊᴏɪɴ', url='https://t.me/Cynitemovies') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('ᴊᴏɪɴ', url='https://t.me/Imdb_updates') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
