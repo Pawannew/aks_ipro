d366a3e727f95b6ee8013ec27574ade487faf810
technical_aks <105633032+technicalaks@users.noreply.github.com>
2022-12-05 10:09:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ class Bot(Client):
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
             workers=50,
-            plugins={"root": "LUCIFER"},
+            plugins={"root": "plugins"},
             sleep_threshold=5,
         )
 
